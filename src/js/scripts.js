c2cc1b9a7f0d13e42a8c46b258132a7054aacd94
dyezer09 <den07y@gmail.com>
2024-05-02 22:51:17 +0300

    add: navbar

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,25 @@
 const buttonId =  document.getElementById("bt");
 const textId =  document.getElementById("txt");
+const navbarId =  document.getElementById("navbar");
+
 
 buttonId.addEventListener("click", () => {
     win.style.display = "block";
   });
   textId.addEventListener("click", () => {
     txt.value = "";
+  });
+
+let onOff = 0;
+
+  navbarId.addEventListener("click", () => {
+    if(onOff == 0){
+      barRow.style.display = "block";
+      onOff = 1;
+    }else{
+      barRow.style.display = "none";
+      onOff = 0;
+    }
+
+    
   });
